5a6bf4dcaf75233bcf894b938153c068cf06a006
Tyler Wray <wraytw@gmail.com>
2023-11-13 09:51:51 -0700

    Adds privacy policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,124 @@
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <p>
+        <strong>Privacy Policy for Minimal Money</strong>
+      </p>
+      <p>Effective Date: Nov 12th, 2023</p>
+      <p>
+        Welcome to Minimal Money! We are committed to protecting your privacy
+        and ensuring the security of your personal information. This Privacy
+        Policy outlines how we collect, use, and safeguard your data when you
+        use our web application. By accessing or using Minimal Money, you agree
+        to the terms and practices described in this policy.
+      </p>
+      <p>
+        <strong>1. Information We Collect</strong>
+      </p>
+      <p>
+        a. <strong>Personal Information:</strong> We may collect personal
+        information such as your name, email address, and other contact details
+        when you register for an account.
+      </p>
+      <p>
+        b. <strong>Financial Information:</strong> To provide budgeting and
+        investment tracking services, we use the Stripe financial connections to
+        gather transaction information from your linked bank accounts.
+      </p>
+      <p>
+        c. <strong>Usage Information:</strong> We may collect information about
+        how you use our web app, including your interactions, preferences, and
+        settings.
+      </p>
+      <p>
+        <strong>2. How We Use Your Information</strong>
+      </p>
+      <p>
+        a. <strong>Providing Services:</strong> We use your personal and
+        financial information to provide you with the budgeting and investment
+        tracking services offered by Minimal Money.
+      </p>
+      <p>
+        b. <strong>Improving Services:</strong> We may use aggregated and
+        anonymized data to analyze usage patterns and improve our services.
+      </p>
+      <p>
+        c. <strong>Communications:</strong> We may use your contact information
+        to send you important updates, announcements, and other communications
+        related to our services. You can opt-out of non-essential
+        communications.
+      </p>
+      <p>
+        <strong>3. How We Protect Your Information</strong>
+      </p>
+      <p>
+        a. <strong>Security Measures:</strong> We implement industry-standard
+        security measures to protect your personal and financial information
+        from unauthorized access, disclosure, alteration, and destruction.
+      </p>
+      <p>
+        b. <strong>Data Encryption:</strong> We use encryption technologies to
+        safeguard sensitive data during transmission.
+      </p>
+      <p>
+        c. <strong>Access Controls:</strong> Access to your personal and
+        financial information is restricted to authorized personnel who need the
+        information to perform their job functions.
+      </p>
+      <p>
+        <strong>4. Financial Connections with Stripe</strong>
+      </p>
+      <p>
+        a. <strong>Authorization:</strong> By using our web app, you authorize
+        us to access and retrieve your transaction information from linked bank
+        accounts through Stripe financial connections.
+      </p>
+      <p>
+        b. <strong>Security:</strong> We do not store your bank login
+        credentials. All sensitive information is securely handled by Stripe.
+      </p>
+      <p>
+        <strong>5. Information Sharing</strong>
+      </p>
+      <p>
+        a. <strong>Third Parties:</strong> We do not sell, trade, or transfer
+        your personal information to third parties for marketing or advertising
+        purposes.
+      </p>
+      <p>
+        b. <strong>Service Providers:</strong> We may share your information
+        with trusted service providers who assist us in providing and improving
+        our services.
+      </p>
+      <p>
+        <strong>6. Your Choices and Rights</strong>
+      </p>
+      <p>
+        a. <strong>Access and Correction:</strong> You have the right to access
+        and correct your personal information. You can update your account
+        information within the Minimal Money settings.
+      </p>
+      <p>
+        b. <strong>Data Deletion:</strong> You can request the deletion of your
+        account and associated data. However, certain data may be retained for
+        legal or legitimate business purposes.
+      </p>
+      <p>
+        <strong>7. Changes to this Privacy Policy</strong>
+      </p>
+      <p>
+        a. <strong>Updates:</strong> We may update this Privacy Policy to
+        reflect changes in our practices or services. We will notify you of any
+        material changes by email or through the web app.
+      </p>
+      <p>
+        <strong>8. Contact Information</strong>
+      </p>
+      <p>
+        If you have any questions or concerns about this Privacy Policy, please
+        contact us at [your contact email].
+      </p>
+      <p>Thank you for using Minimal Money!</p>
+    </>
+  );
+}
